38204754379e2de0110e091899d7adb590512b5f
michael20996 <michael20996@gmail.com>
2020-06-29 14:03:06 -0700

    created server file

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.static('public'));
+//app.use('/api', apiRoutes);
+//app.use('/', htmlRoutes);
+
+//require('./routes/apiRoutes')(app);
+//require('./routes/htmlRoutes')(app);
+
+app.listen(PORT, function(){
+    console.log('App listening on Port:' + PORT);
+})
